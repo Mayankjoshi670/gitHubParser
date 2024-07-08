5758a12ca69feb54f76ddba6f7ba08d977b76f2e
Mayank joshi <147848494+Mayankjoshi670@users.noreply.github.com>
2024-07-09 02:06:01 +0530

    zip and unzip working properly need to resolve error  not able to cleare data  before adding something new

--- backeend/functions/functions.js
+++ backeend/functions/functions.js
@@ -1,48 +1,64 @@
-const http = require('http');
-const experss = require('express') ; 
-const fs = require('fs') ; 
-const path = require('path'); 
+const https = require('https');
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
 const decompress = require('decompress');
 
+const downloadPath = path.join(__dirname, '../download.zip');
+const unzipPath = path.join(__dirname, '../unzipped');
 
-function breakUrl(url){
-    return url.split(/\/{1,2}/).filter(Boolean); 
- }
- 
+function breakUrl(url) {
+    return url.split(/\/{1,2}/).filter(Boolean);
+}
 
- function mergeUrl(userName , projectName){
-    return `https://codeload.github.com/${userName}/${projectName}/zip/refs/heads/master` ; 
- }
+function mergeUrl(userName, projectName) {
+    return `https://codeload.github.com/${userName}/${projectName}/zip/refs/heads/master`;
+}
 
-
- async function dawnloadZip(url){
- const dawnloadPath = path.join(__dirname,'../dawnload.zip');
-    function dawnloadingZip(url , dest){
+async function downloadZip(url) {
+    function downloadingZip(url, dest) {
         return new Promise((resolve, reject) => {
             const file = fs.createWriteStream(dest);
-        
-            https.get(url, (response) => {
-              if (response.statusCode === 200) {
-                response.pipe(file);
-                file.on('finish', () => {
-                  file.close();
-                  resolve(true);
-                });
-              } else {
-                reject(new Error(`Failed to download file: ${response.statusCode}`));
-              }
-            }).on('error', (err) => {
-              // Delete the file in case of an error
-              fs.unlink(dest, () => {
-                reject(new Error(`Failed to write file: ${err.message}`));
-              });
-            });
-          });
 
- }
+            https.get(url, (response) => {
+                if (response.statusCode === 200) {
+                    response.pipe(file);
+                    file.on('finish', () => {
+                        file.close();
+                        resolve(true);
+                        unzip(dest) ; 
+                    });
+                } else {
+                    reject(new Error(`Failed to download file: ${response.statusCode}`));
+                }
+            }).on('error', (err) => {
+                // Delete the file in case of an error
+                fs.unlink(dest, () => {
+                    reject(new Error(`Failed to write file: ${err.message}`));
+                });
+            });
+        });
+    }
+
+    // Wait for the download to complete
+    await downloadingZip(url, downloadPath);
 }
- module.exports = {
+
+async function  unzip(dest){
+
+  try{
+             const file = await decompress(dest , unzipPath) ; 
+             console.log("unzip the file") ; 
+         }
+         catch(error){
+             console.log("error generated" + error) ; 
+         }
+
+}
+
+module.exports = {
     breakUrl,
     mergeUrl,
-    dawnloadZip
- }
+    downloadZip,
+    unzip
+};
--- backeend/routs/index.js
+++ backeend/routs/index.js
@@ -1,28 +1,27 @@
 const express = require('express');
 const router = express.Router();
- const {breakUrl} = require('../functions/functions')
- const {mergeUrl} = require('../functions/functions')
- 
-router.post('/url', (req, res) => {  
+const { breakUrl, mergeUrl, downloadZip, unzip } = require('../functions/functions'); // Corrected import
+
+router.post('/url', async (req, res) => {  // Ensure async usage here
     const url = req.body.url;
     console.log(url);
 
-    const urlArray = breakUrl(url) ;
+    const urlArray = breakUrl(url);
     
-    const userName = urlArray[2]  ; 
-    const projectName = urlArray[3] ; 
-    const updatedUrl = mergeUrl(userName ,projectName) ;  
+    const userName = urlArray[2];
+    const projectName = urlArray[3];
+    const updatedUrl = mergeUrl(userName, projectName);
 
     console.log(updatedUrl);
 
-//    const flag = dawnloadZip(updatedUrl) ; 
-//     if(flag){
-//         console.log("Download successful") ;
-//         unzip() ; 
-//     }
-//     else{
-//         console.log("dawnload failed") ; 
-//     }
+    try {
+        await downloadZip(updatedUrl); // Await downloadZip because it's async
+        console.log("Download successful");
+        // await unzip(downloadPath); // Ensure you pass the correct downloadPath if needed
+        console.log("Unzip successful");
+    } catch (error) {
+        console.error("Download or unzip failed:", error);
+    }
 
     res.json({
         receivedUrl: url
